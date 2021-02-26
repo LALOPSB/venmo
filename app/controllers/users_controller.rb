a699cbd0a04661687c3a3c105a656ede22a2acf2
lalo <lalopsb@hotmail.com>
2021-02-25 23:39:52 -0300

    adds user not found error on endpoints

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,36 @@
 class UsersController < ApplicationController
+  before_action :find_user
+
   def payment
     friend = User.find(user_params[:friend_id])
-    User.find(params[:id]).send_payment(friend, user_params[:amount].to_i, user_params[:description])
+    user.send_payment(friend, user_params[:amount].to_i, user_params[:description])
 
     render json: {}, status: 200
-  rescue User::PaymentAmountOutOfBounds, User::NonFriendPaymentAttempt => e
+  rescue User::PaymentAmountOutOfBounds, User::NonFriendPaymentAttempt, ActiveRecordError => e
     render json: { error: e.message }, status: 400
   end
 
   def balance
-    balance = User.find(params[:id]).payment_account.balance
+    balance = user.payment_account.balance
 
     render json: { balance_check: balance }, status: 200
   end
 
   def feed
-    feed_items = UserFeedBuilder.new(User.find(params[:id]), user_params[:page]).execute
+    feed_items = UserFeedBuilder.new(user, user_params[:page]).execute
 
     render json: { activity_feed: feed_items }, status: 200
   end
 
   private
+
+  def find_user
+    render json: { error: 'User not found!' }, status: :not_found and return unless user
+  end
+
+  def user
+    @user ||= User.find_by(id: params[:id])
+  end
 
   def user_params
     params.permit(:friend_id, :amount, :description, :page)
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,56 +3,71 @@ require 'rails_helper'
 RSpec.describe 'users endpoints', type: :request do
   let(:json_body) { JSON.parse(response.body).deep_symbolize_keys }
   let(:user) { FactoryBot.create(:user) }
+  let(:user_id) { user.id }
   let(:friend) { FactoryBot.create(:user) }
   let(:amount) { 100 }
   let(:description) { 'This is a payment' }
   let!(:user_account) { FactoryBot.create(:payment_account, balance: 130, user: user) }
   let!(:friend_account) { FactoryBot.create(:payment_account, balance: 180, user: friend) }
 
-  describe 'POST payment' do
-    subject(:send_payment) { post "/user/#{user.id}/payment?friend_id=#{friend.id}&amount=#{amount}&description=#{description}" }
+  shared_examples 'raises user not found error for non existent user' do
+    context 'non existent user' do
+      let(:user_id) { 9999 }
 
-    context 'when a user sends a payment' do
+      it 'returns a not found error' do
+        subject
 
-      context 'to a friend' do
-        before { Friendship.create!(user: user, friend: friend) }
-
-        context 'with an amount within bounds' do
-          it 'returns a successful response and sends payment' do
-            send_payment
-
-            expect(response.status).to eq(200)
-            expect(json_body).to eq({})
-            expect(user_account.reload.balance).to eq(30)
-            expect(friend_account.reload.balance).to eq(280)
-          end
-        end
-
-        context 'with an amount outside bounds' do
-          let(:amount) { 1001 }
-
-          it 'returns an error' do
-            send_payment
-
-            expect(response.status).to eq(400)
-            expect(json_body).to eq({ error: 'Payment amount should be between 0 and 1000' })
-          end
-        end
-      end
-
-      context 'to a non friend' do
-        it 'returns an error' do
-          send_payment
-
-          expect(response.status).to eq(400)
-          expect(json_body).to eq({ error: 'You can make payments only to befriended users' })
-        end
+        expect(response.status).to eq(404)
+        expect(json_body).to eq({ error: 'User not found!'})
       end
     end
   end
 
+  describe 'POST payment' do
+    subject(:send_payment) { post "/user/#{user_id}/payment?friend_id=#{friend.id}&amount=#{amount}&description=#{description}" }
+
+    context 'when a user sends a payment' do
+        context 'to a friend' do
+          before { Friendship.create!(user: user, friend: friend) }
+
+          context 'with an amount within bounds' do
+            it 'returns a successful response and sends payment' do
+              send_payment
+
+              expect(response.status).to eq(200)
+              expect(json_body).to eq({})
+              expect(user_account.reload.balance).to eq(30)
+              expect(friend_account.reload.balance).to eq(280)
+            end
+          end
+
+          context 'with an amount outside bounds' do
+            let(:amount) { 1001 }
+
+            it 'returns an error' do
+              send_payment
+
+              expect(response.status).to eq(400)
+              expect(json_body).to eq({ error: 'Payment amount should be between 0 and 1000' })
+            end
+          end
+        end
+
+        context 'to a non friend' do
+          it 'returns an error' do
+            send_payment
+
+            expect(response.status).to eq(400)
+            expect(json_body).to eq({ error: 'You can make payments only to befriended users' })
+          end
+        end
+      end
+
+    it_behaves_like 'raises user not found error for non existent user'
+  end
+
   describe 'GET balance' do
-    subject(:get_balance_check) { get "/user/#{user.id}/balance" }
+    subject(:get_balance_check) { get "/user/#{user_id}/balance" }
 
     it 'returns a successful response and sends payment' do
       get_balance_check
@@ -60,12 +75,14 @@ RSpec.describe 'users endpoints', type: :request do
       expect(response.status).to eq(200)
       expect(json_body).to eq({ balance_check: 130 })
     end
+
+    it_behaves_like 'raises user not found error for non existent user'
   end
 
   describe 'GET feed' do
     include_context 'activity feed'
 
-    subject(:get_feed) { get "/user/#{user.id}/feed" }
+    subject(:get_feed) { get "/user/#{user_id}/feed" }
 
     let(:expected_user_activity_feed) do
       [
@@ -84,17 +101,18 @@ RSpec.describe 'users endpoints', type: :request do
     end
 
     context 'pagination' do
-
       before { stub_const("#{UserFeedBuilder}::DEFAULT_PER_PAGE_ITEMS", 3) }
 
       let(:expected_user_activity_feed_page_2) { [pay_to_friend_title_and_desc] }
       let(:page) { 2 }
 
       it 'returns items for the page provided' do
-        get "/user/#{user.id}/feed?page=#{page}"
+        get "/user/#{user_id}/feed?page=#{page}"
 
         expect(json_body).to eq({ activity_feed: expected_user_activity_feed_page_2 })
       end
     end
+
+    it_behaves_like 'raises user not found error for non existent user'
   end
 end
